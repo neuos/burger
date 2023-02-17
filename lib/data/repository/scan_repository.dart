06f5af06fb0200181144849dd7d0547f35ac635d
neuos <github@neuhuber.eu>
2023-02-17 15:07:10 +0100

    design

--- lib/data/repository/scan_repository.dart
+++ lib/data/repository/scan_repository.dart
@@ -1,24 +1,28 @@
 import 'package:get_it/get_it.dart';
+import 'package:logger/logger.dart';
 import 'package:sqflite/sqflite.dart';
 import '../model/scan.dart';
 
 abstract class IScanRepository {
   Future<void> insert(Scan scan);
   Future<List<Scan>> find(String tagId);
+  Future<List<Scan>> findAll();
 }
 
 
 class ScanRepository implements IScanRepository {
   final _db = GetIt.I.getAsync<Database>();
+  final logger = Logger();
 
   @override
   Future<void> insert(Scan scan) async {
     final db = await _db;
-    await db.insert(
+    final res = await db.insert(
       Scan.tableName,
       scan.toMap(),
-      conflictAlgorithm: ConflictAlgorithm.replace,
+      conflictAlgorithm: ConflictAlgorithm.ignore,
     );
+    logger.d("inserted $res");
   }
 
   @override
@@ -28,7 +32,17 @@ class ScanRepository implements IScanRepository {
       Scan.tableName,
       where: 'tagId = ?',
       whereArgs: [tagId],
+      orderBy: 'timestamp ASC',
     );
+    logger.d("found ${res.length} results");
+    return res.map(Scan.fromMap).toList();
+  }
+
+  @override
+  Future<List<Scan>> findAll() async {
+    final db = await _db;
+    var res = await db.query(Scan.tableName, orderBy: 'timestamp ASC');
+    logger.d("found ${res.length} results");
     return res.map(Scan.fromMap).toList();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:burger/data/model/scan.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -10,13 +12,18 @@ import 'package:sqflite/utils/utils.dart';
 
 import 'data/database.dart';
 import 'data/repository/scan_repository.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final getIt = GetIt.instance;
+  final getIt = GetIt.I;
   getIt.registerLazySingletonAsync<Database>(() => DatabaseProvider.initDB());
   getIt.registerLazySingleton<IScanRepository>(() => ScanRepository());
+  getIt.registerLazySingleton(() => ThemeData(
+        colorSchemeSeed: const Color(0xFFF29400),
+        useMaterial3: true,
+      ));
 
   runApp(const MyApp());
 }
@@ -26,11 +33,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var themeData = GetIt.I.get<ThemeData>();
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
+      theme: themeData,
       home: const MyHomePage(title: 'Burger Scanner'),
     );
   }
@@ -38,6 +44,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -50,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   String? _error;
   List<DateTime> _history = [];
-  Uint8List? previousId;
+  String? previousId;
 
   @override
   initState() {
@@ -67,10 +74,10 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> _startScanner() async {
     bool isAvailable = await NfcManager.instance.isAvailable();
     if (!isAvailable) {
-      logger.w('nfc reader not available');
       setState(() {
         _error = "nfc reader not available";
       });
+      logger.w(_error);
       return;
     }
 
@@ -86,47 +93,47 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> onDiscovered(NfcTag tag) async {
-    final id = getId(tag);
-
     _error = null;
     _history = [];
 
+    final id = getId(tag);
+
     // ignore double scans
-    if (listEquals(id, previousId)) {
+    if (id == previousId) {
       return;
     }
     previousId = id;
 
     if (id == null) {
-      logger.w("No id found", tag.data);
       setState(() {
-        _error = "no id found";
+        _error = "No ID found";
       });
+      logger.w(_error, tag.data);
       return;
     }
 
-    final stringId = hex(id);
-    logger.i('read id: $stringId');
+    logger.i('read ID: $id');
 
-    final history = await repo.find(stringId);
-    repo.insert(Scan(tagId: stringId));
-
-    if (history.isNotEmpty) {
-      logger.i('already scanned: $history');
-      setState(() {
-        _error = "already scanned";
-        _history = history.map((s) => s.timestamp).toList();
-      });
-    } else {
-      setState(() {
-        _error = null;
-        _history = [];
-      });
-    }
+    await repo.insert(Scan(tagId: id));
+    final history = await repo.find(id);
+    setState(() {
+      _history = history.map((e) => e.timestamp).toList();
+    });
   }
 
-  Uint8List? getId(NfcTag tag) {
-    return NdefFormatable.from(tag)?.identifier ?? MiFare.from(tag)?.identifier;
+  String? getId(NfcTag tag) {
+    final id = NfcA.from(tag)?.identifier ??
+        NfcB.from(tag)?.identifier ??
+        NfcF.from(tag)?.identifier ??
+        NfcV.from(tag)?.identifier ??
+        MiFare.from(tag)?.identifier ??
+        MifareClassic.from(tag)?.identifier ??
+        MifareUltralight.from(tag)?.identifier ??
+        NdefFormatable.from(tag)?.identifier ??
+        Iso7816.from(tag)?.identifier ??
+        Iso15693.from(tag)?.identifier;
+
+    return id != null ? hex(id) : null;
   }
 
   @override
@@ -136,33 +143,173 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            if (_history.isEmpty && _error == null)
-              const Text(
-                'Not scanned yet',
-                style: TextStyle(color: Colors.green),
+        child: Column(children: <Widget>[
+          ScanResult(history: _history, error: _error),
+          Expanded(child: Container()),
+          const Statistic(),
+        ]),
+      ),
+    );
+  }
+}
+
+class Statistic extends StatelessWidget {
+  const Statistic({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Scan>>(
+      future: GetIt.I.get<IScanRepository>().findAll(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<int> counts = getBinned(snapshot.data!);
+
+          Logger().i('counts: $counts');
+
+          final chart = LineChart(LineChartData(
+            lineBarsData: [
+              LineChartBarData(
+                spots: counts
+                    .asMap()
+                    .entries
+                    .map((e) => FlSpot(e.key.toDouble(), e.value.toDouble()))
+                    .toList(),
               ),
-            if (_error != null)
-              Text(
-                _error!,
-                style: const TextStyle(color: Colors.red),
+            ],
+          ));
+
+          return Column(
+            children: [
+              Text('History (${snapshot.data!.length})'),
+              AspectRatio(aspectRatio: 1.5, child: chart),
+            ],
+          );
+        } else {
+          return const Text('Loading...');
+        }
+      },
+    );
+  }
+
+  List<int> getBinned(List<Scan> data, {int segments = 16}) {
+    if (data.isEmpty) {
+      Logger().w('no data');
+      return List.filled(segments, 0);
+    }
+    final first = data.first.timestamp;
+    final last = data.last.timestamp.add(const Duration(seconds: 1));
+    final diff = last.difference(first).inSeconds;
+    final segment = diff / segments;
+    final counts = List.filled(segments, 0);
+    for (final scan in data) {
+      final index = scan.timestamp.difference(first).inSeconds ~/ segment;
+      counts[index]++;
+    }
+    return counts;
+  }
+}
+
+class ScanResult extends StatelessWidget {
+  const ScanResult({super.key, required this.history, this.error});
+
+  final List<DateTime> history;
+  final String? error;
+
+  @override
+  Widget build(BuildContext context) {
+    final Status status;
+    final String text;
+    if (error != null) {
+      status = Status.error;
+      text = error!;
+    } else if (history.length == 1) {
+      status = Status.success;
+      text = "Enjoy your Burger";
+    } else {
+      status = Status.warn;
+      text = "Scanned ${history.length} times";
+    }
+
+    return Column(
+      children: [
+        Column(
+          children: [
+            StatusCard(
+              status: status,
+              text: text,
+            ),
+            SizedBox(
+              height: 300,
+              child: ListView.builder(
+                shrinkWrap: false,
+                itemCount: history.length,
+                itemBuilder: (context, index) {
+                  final date = history[index];
+                  return Card(
+                    child: ListTile(
+                      title: Text(date.toString()),
+                    ),
+                  );
+                },
               ),
-            if (_history.isNotEmpty)
-              Column(
-                children: [
-                  Text(
-                    'Scanned ${_history.length} times',
-                    style: const TextStyle(color: Colors.red),
-                  ),
-                  for (final date in _history)
-                    Text(
-                      date.toString(),
-                      style: const TextStyle(color: Colors.red),
-                    )
-                ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+enum Status { success, warn, error }
+
+class StatusCard extends StatelessWidget {
+  const StatusCard({super.key, required this.status, required this.text});
+
+  final Status status;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    final Color containerColor;
+    final IconData icon;
+    final Color color;
+
+    var themeData = GetIt.I.get<ThemeData>();
+    switch (status) {
+      case Status.success:
+        containerColor = themeData.colorScheme.primaryContainer;
+        icon = Icons.check_box;
+        color = themeData.colorScheme.primary;
+        break;
+      case Status.warn:
+        containerColor = themeData.colorScheme.errorContainer;
+        icon = Icons.warning;
+        color = themeData.colorScheme.error;
+        break;
+      case Status.error:
+        containerColor = themeData.colorScheme.errorContainer;
+        icon = Icons.error;
+        color = themeData.colorScheme.error;
+        break;
+    }
+
+    return Card(
+      color: containerColor,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Icon(
+              icon,
+              color: color,
+              size: 128,
+            ),
+            Flexible(
+              child: Text(
+                text,
+                style: TextStyle(color: color, fontSize: 32),
               ),
+            ),
           ],
         ),
       ),
