bfa315d3fa32cfd7c4e5fc743e598f43bd31f868
neuos <github@neuhuber.eu>
2023-02-18 17:10:21 +0100

    fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,25 +5,26 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:burger/data/database.dart';
+import 'package:burger/data/repository/event_repository.dart';
+import 'package:burger/data/repository/scan_repository.dart';
 import 'package:burger/main.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  final getIt = GetIt.instance;
+
+  setUpAll(() {
+    getIt.registerLazySingletonAsync<Database>(() => DatabaseProvider.initDB());
+    getIt.registerLazySingleton<IScanRepository>(() => ScanRepository());
+    getIt.registerLazySingleton<IEventRepository>(() => EventRepository());
+  });
+
+  testWidgets('App runs and has Widgets', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(tester.allWidgets.isNotEmpty, true);
   });
 }
